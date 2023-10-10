a5f4c64160928ec19cc2449aa7b46e2634e14122
iksadNorth <iksadNorth@gmail.com>
2023-10-10 15:27:10 +0900

    Revert ""Fix : 팔로우 관련 조회 API 수정""
    This reverts commit 90da4adb

--- src/main/java/com/example/demo/follow/controller/FollowController.java
+++ src/main/java/com/example/demo/follow/controller/FollowController.java
@@ -2,11 +2,8 @@ package com.example.demo.follow.controller;
 
 import com.example.demo.follow.dto.FollowMemberResponseDto;
 import com.example.demo.follow.service.FollowService;
-import com.example.demo.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,22 +15,14 @@ import java.util.List;
 public class FollowController {
     private final FollowService followService;
 
-    @PostMapping("/api/shops/{shopId}/follows")
-    public ResponseEntity<Void> doShopFollow(
-            @PathVariable Long shopId,
-            @AuthenticationPrincipal UserDetailsImpl principal
-            ) {
-        return followService.doShopFollow(principal.getMember() , shopId);
-    }
-
-    @GetMapping("/api/members/{memberId}/followers")
+    @PostMapping("/api/members/{memberId}/followers")
     public ResponseEntity<List<FollowMemberResponseDto>> readFollowersByMemberId(
             @PathVariable Long memberId
     ) {
         return followService.readFollowersByMemberId(memberId);
     }
 
-    @GetMapping("/api/members/{memberId}/followings")
+    @PostMapping("/api/members/{memberId}/followings")
     public ResponseEntity<List<FollowMemberResponseDto>> readFollowingsByMemberId(
             @PathVariable Long memberId
     ) {
