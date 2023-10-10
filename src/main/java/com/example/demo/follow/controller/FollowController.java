392cf81ed1a9214e197429dd47496e3229ba14fa
iksadNorth <iksadNorth@gmail.com>
2023-10-10 15:24:26 +0900

    "Feat : 팔로우 기능 API 구현"
    상점에 대한 팔로우 기능 API 구현
    
    (cherry picked from commit 37f5192a325b79b17010680806070755e398c94d)

--- src/main/java/com/example/demo/follow/controller/FollowController.java
+++ src/main/java/com/example/demo/follow/controller/FollowController.java
@@ -2,10 +2,13 @@ package com.example.demo.follow.controller;
 
 import com.example.demo.follow.dto.FollowMemberResponseDto;
 import com.example.demo.follow.service.FollowService;
+import com.example.demo.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +17,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FollowController {
     private final FollowService followService;
+
+    @PostMapping("/api/shops/{shopId}/follows")
+    public ResponseEntity<Void> doShopFollow(
+            @PathVariable Long shopId,
+            @AuthenticationPrincipal UserDetailsImpl principal
+            ) {
+        return followService.doShopFollow(principal.getMember() , shopId);
+    }
 
     @GetMapping("/api/members/{memberId}/followers")
     public ResponseEntity<List<FollowMemberResponseDto>> readFollowersByMemberId(
