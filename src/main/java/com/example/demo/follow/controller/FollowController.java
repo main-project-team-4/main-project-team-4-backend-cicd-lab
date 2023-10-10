90da4adbd17676cb0196e22eff07b8e265bf5348
iksadNorth <iksadNorth@gmail.com>
2023-10-10 13:29:53 +0900

    "Fix : 팔로우 관련 조회 API 수정"
    Post로 요구했던 조회 API를 Get으로 변경

--- src/main/java/com/example/demo/follow/controller/FollowController.java
+++ src/main/java/com/example/demo/follow/controller/FollowController.java
@@ -4,8 +4,8 @@ import com.example.demo.follow.dto.FollowMemberResponseDto;
 import com.example.demo.follow.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,14 +15,14 @@ import java.util.List;
 public class FollowController {
     private final FollowService followService;
 
-    @PostMapping("/api/members/{memberId}/followers")
+    @GetMapping("/api/members/{memberId}/followers")
     public ResponseEntity<List<FollowMemberResponseDto>> readFollowersByMemberId(
             @PathVariable Long memberId
     ) {
         return followService.readFollowersByMemberId(memberId);
     }
 
-    @PostMapping("/api/members/{memberId}/followings")
+    @GetMapping("/api/members/{memberId}/followings")
     public ResponseEntity<List<FollowMemberResponseDto>> readFollowingsByMemberId(
             @PathVariable Long memberId
     ) {
