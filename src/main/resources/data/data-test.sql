80da6bd68aa551fb147f12a0e21432dc90243007
iksadNorth <iksadNorth@gmail.com>
2023-10-12 20:14:43 +0900

    "Fix : 카테고리 Testcase 추가"
    카테고리 Testcase 추가

--- src/main/resources/data/data-test.sql
+++ src/main/resources/data/data-test.sql
@@ -1,10 +1,30 @@
-insert into category_large (id, name) values (1, 'man');
-insert into category_large (id, name) values (2, 'woman');
+insert into category_large (id, name) values (1, '여성의류');
+insert into category_mid (id, name, parent) VALUES (1, '아우터', 1);
+insert into category_mid (id, name, parent) VALUES (2, '상의', 1);
+insert into category_mid (id, name, parent) VALUES (3, '하의', 1);
+insert into category_mid (id, name, parent) VALUES (4, '원피스', 1);
+insert into category_mid (id, name, parent) VALUES (5, '신발', 1);
 
-insert into category_mid (id, name, parent) VALUES (1, 'man shirt', 1);
-insert into category_mid (id, name, parent) VALUES (2, 'man pants', 1);
-insert into category_mid (id, name, parent) VALUES (3, 'woman shirt', 2);
-insert into category_mid (id, name, parent) VALUES (4, 'woman pants', 2);
+insert into category_large (id, name) values (2, '남성의류');
+insert into category_mid (id, name, parent) VALUES (6, '아우터', 2);
+insert into category_mid (id, name, parent) VALUES (7, '상의', 2);
+insert into category_mid (id, name, parent) VALUES (8, '하의', 2);
+insert into category_mid (id, name, parent) VALUES (9, '신발', 2);
+
+insert into category_large (id, name) values (3, '패션잡화');
+insert into category_mid (id, name, parent) VALUES (10, '가방', 3);
+insert into category_mid (id, name, parent) VALUES (11, '지갑', 3);
+insert into category_mid (id, name, parent) VALUES (12, '시계', 3);
+insert into category_mid (id, name, parent) VALUES (13, '모자', 3);
+insert into category_mid (id, name, parent) VALUES (14, '안경/선글라스', 3);
+insert into category_mid (id, name, parent) VALUES (15, '기타', 3);
+
+insert into category_large (id, name) values (4, '쥬얼리');
+insert into category_mid (id, name, parent) VALUES (16, '목걸이', 4);
+insert into category_mid (id, name, parent) VALUES (17, '반지', 4);
+insert into category_mid (id, name, parent) VALUES (18, '귀걸이/피어싱', 4);
+insert into category_mid (id, name, parent) VALUES (19, '팔찌', 4);
+insert into category_mid (id, name, parent) VALUES (20, '기타', 4);
 
 insert into item (name, price, comment, category_mid_id) values ('jacket1', 10000, 'nice man shirt1', 1);
 insert into item (name, price, comment, category_mid_id) values ('jacket2', 5000, 'nice man shirt2', 1);
